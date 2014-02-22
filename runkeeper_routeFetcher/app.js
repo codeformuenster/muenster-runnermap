e5db3f2f19df8e07e4966253962335a53e2dd58c
dwilhelm89 <dwilhelm89@googlemail.com>
2014-02-22 13:43:18 +0100

    download route and convert to geojson line

--- runkeeper_routeFetcher/app.js
+++ runkeeper_routeFetcher/app.js
@@ -2,26 +2,54 @@ var request = require('request');
 var fs = require('fs');
 
 
-request('http://runkeeper.com/user/1326910888/route', function(error, response, body) {
-    if (!error && response.statusCode == 200) {
-        var start = body.indexOf('var routePoints =');
-        var end = body.indexOf('];', start);
-        console.log("Start: " + start + "End: " + end);
+getRoute('http://runkeeper.com/user/1326910888/route');
 
-        var route = body.substring(start + 18, end + 1);
-        writeToFile("route.json", route);
-    } else {
-        console.log(error);
 
-    }
-});
+function getRoute(url) {
+    request(url, function(error, response, body) {
+        if (!error && response.statusCode == 200) {
+            var start = body.indexOf('var routePoints =');
+            var end = body.indexOf('];', start);
+            console.log("Start: " + start + "End: " + end);
 
-function writeToFile(fileName, content){
-	fs.writeFile("./routes/"+fileName, content, function(err) {
-    if(err) {
-        console.log(err);
-    } else {
-        console.log("The file was saved!");
-    }
-}); 
+            var route = body.substring(start + 18, end + 1);
+            var jsonRoute = JSON.parse(route);
+
+            var geojsonRoute = convertToGeojsonLine(jsonRoute);
+            writeToFile('route.json', JSON.stringify(geojsonRoute));
+
+        } else {
+            console.log(error);
+
+        }
+    });
+}
+
+function writeToFile(fileName, content) {
+    fs.writeFile("./routes/" + fileName, content, function(err) {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log("The file was saved!");
+        }
+    });
+}
+
+
+function convertToGeojsonLine(route) {
+    var geojsonRoute = {
+        "type": "Feature",
+        "properties": {},
+        "geometry": {
+            "type": "LineString",
+            "coordinates": []
+        }
+    };
+
+    route.forEach(function(routeSegment) {
+        geojsonRoute.geometry.coordinates.push([routeSegment.longitude, routeSegment.latitude]);
+    });
+
+    return geojsonRoute;
+
 }
